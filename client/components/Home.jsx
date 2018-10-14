e9987c2c74cd78604de69c06946c9e06c5192d57
lukechadwick <llukechadwick@gmail.com>
2018-10-14 22:18:51 +1300

    css tidyups

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -4,19 +4,17 @@ import { Route, Link } from 'react-router-dom';
 const Home = () => {
   return (
     <React.Fragment>
-      <div className="row">
-        <div className="col-lg-12">
-          <div id="content">
-            <h1>CodeProject VPN</h1>
-            <h3>Secure VPN Powered by Open-Source Tech</h3>
-            <hr />
-            <Link to="/setup">
-              <button className="btn btn-default btn-lg">
-                <i className="fa fa-shield mr-2" aria-hidden="true" />
-                Get Started
-              </button>
-            </Link>
-          </div>
+      <div className="row fix">
+        <div className="col-sm content ">
+          <h1>CodeProject VPN</h1>
+          <h3>Secure VPN Powered by Open-Source Tech</h3>
+          <hr />
+          <Link to="/setup">
+            <button className="btn btn-default btn-lg">
+              <i className="fa fa-shield mr-2" aria-hidden="true" />
+              Get Started
+            </button>
+          </Link>
         </div>
       </div>
     </React.Fragment>
--- client/components/Setup.jsx
+++ client/components/Setup.jsx
@@ -6,58 +6,48 @@ const Setup = () => {
     <div className="container">
       <div className="jumbotron">
         <div class="row flex">
-          <div class="col-lg-6">
-            <div id="content">
-              <h1>Client Download</h1>
-              <h3>
-                Visit the Shadowsocks Github page to download the client for
-                your device
-              </h3>
-              <hr />
-              <a href="https://shadowsocks.org/en/download/clients.html">
-                <button class="btn btn-default btn-lg">
-                  <i class="fa fa-download  mr-2" aria-hidden="true" /> Download
-                </button>
-              </a>
-            </div>
+          <div class="col-lg-6 content">
+            <h1>Client Download</h1>
+            <h3>
+              Visit the Shadowsocks Github page to download the client for your
+              device
+            </h3>
+            <hr />
+            <a href="https://shadowsocks.org/en/download/clients.html">
+              <button class="btn btn-default btn-lg">
+                <i class="fa fa-download  mr-2" aria-hidden="true" /> Download
+              </button>
+            </a>
           </div>
-          <div class="col-lg-6">
-            <div id="content">
-              <h1>Configure Client</h1>
-              <h3>Setup your devices client using the instructions below:</h3>
-              <hr />
-              <a href="https://shadowsocks.org/en/download/clients.html">
-                <button class="btn btn-default btn-lg">
-                  <i class="fa fa-cogs  mr-2" aria-hidden="true" />{' '}
-                  Configuration
-                </button>
-              </a>
-            </div>
+          <div class="col-lg-6 content">
+            <h1>Configure Client</h1>
+            <h3>Setup your devices client using the instructions below:</h3>
+            <hr />
+            <a href="https://shadowsocks.org/en/download/clients.html">
+              <button class="btn btn-default btn-lg">
+                <i class="fa fa-cogs  mr-2" aria-hidden="true" /> Configuration
+              </button>
+            </a>
           </div>
-          <div class="col-lg-6">
-            <div id="content">
-              <h1>Manual Install</h1>
-              <h3>
-                We also host versions here but these may not be the most
-                recent/secure builds:
-              </h3>
-              <hr />
-              <a href="[4.1.8]com.github.shadowsocks.apk">
-                <i
-                  class="fa fa-android fa-4x m-4 text-success"
-                  aria-hidden="true"
-                />
-              </a>
-              <a href="Shadowsocks-4.0.7.zip">
-                <i class="fa fa-windows fa-4x m-4" aria-hidden="true" />
-              </a>
-              <a href="https://shadowsocks.org/en/download/clients.html">
-                <i
-                  class="fa fa-mobile fa-4x m-4 text-dark"
-                  aria-hidden="true"
-                />
-              </a>
-            </div>
+          <div class="col-lg-6 content">
+            <h1>Manual Install</h1>
+            <h3>
+              We also host versions here but these may not be the most
+              recent/secure builds:
+            </h3>
+            <hr />
+            <a href="[4.1.8]com.github.shadowsocks.apk">
+              <i
+                class="fa fa-android fa-4x m-4 text-success"
+                aria-hidden="true"
+              />
+            </a>
+            <a href="Shadowsocks-4.0.7.zip">
+              <i class="fa fa-windows fa-4x m-4" aria-hidden="true" />
+            </a>
+            <a href="https://shadowsocks.org/en/download/clients.html">
+              <i class="fa fa-mobile fa-4x m-4 text-dark" aria-hidden="true" />
+            </a>
           </div>
         </div>
       </div>
