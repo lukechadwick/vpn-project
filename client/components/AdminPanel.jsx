eb563568f2ddffe0de32b004db62c60ff7cd5f14
lukechadwick <llukechadwick@gmail.com>
2019-02-27 14:00:03 +1300

    create admin panel, will be used for reading out hardware info from the server using graphQL queries

---
+++ client/components/AdminPanel.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AdminPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>Graph Code here</div>;
+  }
+}
+
+export default AdminPanel;
